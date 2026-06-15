665cf66b9c5f6333b5be6c0e2c112013f708135b
Daley <tadaley@ndc.nasa.gov>
2026-06-15 15:52:38 -0400

    Forgot to comment out the print on test_rcbuilder

--- aviary/subsystems/propulsion/rc_electric/test/test_rcbuilder.py
+++ aviary/subsystems/propulsion/rc_electric/test/test_rcbuilder.py
@@ -64,4 +64,4 @@ esc_power = prob.get_val('rc_electric.esc.power', units='W')
 motor_power = prob.get_val('rc_electric.motor.power', units='W')
 prop_power = prob.get_val(Dynamic.Vehicle.Propulsion.PROP_POWER, units='W')
 power_residual = battery_power + esc_power + motor_power - prop_power
-print("Bat_pow W", battery_power, "Esc_Pow W", esc_power,"Motor_Pow W", motor_power, "Prop_Pow W", prop_power)
+#print("Bat_pow W", battery_power, "Esc_Pow W", esc_power,"Motor_Pow W", motor_power, "Prop_Pow W", prop_power)
